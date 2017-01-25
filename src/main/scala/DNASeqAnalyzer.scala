8da0d3d5ce4911e1c240b152270d289fab3dc011
Najeeb Aslam <muhammad-najeeb.aslam@etudiant.univ-rennes1.fr>
2017-01-25 03:00:31 +0100

    Update DNASeqAnalyzer.scala

--- src/main/scala/DNASeqAnalyzer.scala
+++ src/main/scala/DNASeqAnalyzer.scala
@@ -15,7 +15,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  * 
- * Authors: Hamid Mushtaq
+ * Authors: Najeeb Aslam
  *
 */
 import org.apache.spark.SparkContext
